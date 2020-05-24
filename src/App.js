aadfe0ae319965ac34dc60cfec545934bb73b852
Francesca Chater <francescachater@francescas-mbp.home>
2020-05-24 10:58:44 +0100

    Avatar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import NavBar from './components/layout/NavBar'
+import UserItem from './components/users/UserItem'
 
 class App extends Component{
   render() {
     return (
       <div className="App">
         <NavBar/>     
+        <UserItem />
       </div>    
     );
   }
---
+++ src/components/users/UserItem.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+class UserItem extends Component {
+    constructor(){
+        super();
+        this.state = {
+            id: 'id',
+            login: 'mojombo',
+            avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+            html_url: 'https://github.com/mojombo'
+        }
+    }
+    render() {
+        return (
+            <div className="card text-center">
+               <img 
+                src={this.state.avatar_url} 
+                alt='' 
+                className='round-img' style={{width: '60px'}}
+               />
+            </div>
+        );
+    }
+}
+
+export default UserItem
