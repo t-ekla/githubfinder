97f525cd926da7352b767be148717a1f526f3520
Francesca Chater <francescachater@gmail.com>
2020-05-25 09:13:52 +0100

    Using axios, async and await syntax to call github

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/NavBar';
 import Users from './components/users/Users';
+import axios from 'axios';
 
-class App extends Component{
+class App extends Component {
+  async componentDidMount() {
+    const response = await axios.get('https://api.github.com/users')    
+    console.log(response.data)
+  }
+  
+
   render() {
     return (
       <div className="App">
