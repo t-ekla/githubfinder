4cb87c653d25b6cc18fccdc0acb4e6a910592851
Francesca Chater <francescachater@gmail.com>
2020-05-25 09:22:31 +0100

    Removing hardcoded users from Users state, passing in users as props from app js

--- src/App.js
+++ src/App.js
@@ -5,18 +5,25 @@ import Users from './components/users/Users';
 import axios from 'axios';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  }
+
   async componentDidMount() {
+    this.setState({ loading: true });
+
     const response = await axios.get('https://api.github.com/users')    
-    console.log(response.data)
+    this.setState({ users: response.data, loading: false })
+
   }
   
-
   render() {
     return (
       <div className="App">
         <Navbar />  
         <div className='container'/>   
-        <Users />
+        <Users loading={this.state.loading} users={this.state.users} />
       </div>    
     );
   }
