b7c295fb444f6c77a6ac0b07282eccce6c0ce821
Francesca Chater <francescachater@gmail.com>
2020-06-15 14:52:09 +0100

    Set popup alert

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/NavBar';
 import Users from './components/users/Users';
-import Search from './components/users/Search'
+import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 import axios from 'axios';
 
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   }
 
  
@@ -23,16 +25,31 @@ class App extends Component {
   // Clear users from state
   clearUsers = () => this.setState({ users: [], loading: false });
   
+  // Set Alert 
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+
+    setTimeout(() => this.setState({ alert: null}), 5000)
+
+  } 
+
+
   render() {
+    const { users, loading } = this.state;
     return (
-      <div className="App">
+      <div className='App'>
         <Navbar />  
-        <div className='container'/>   
-        <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={
-          this.state.users.length > 0 ? true : false
-        }/>
-        <Users loading={this.state.loading} users={this.state.users} />
-      </div>    
+        <div className='container'> 
+        <Alert alert={this.state.alert} />  
+        <Search 
+          searchUsers={this.searchUsers} 
+          clearUsers={this.clearUsers} 
+          showClear={ users.length > 0 ? true : false }
+          setAlert={this.setAlert}
+          />
+        <Users loading={loading} users={users} />
+      </div> 
+      </div>   
     );
   }
 }
