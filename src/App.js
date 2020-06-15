272d2cc4294cf030c611f403d0ae975f48d7e116
Francesca Chater <francescachater@gmail.com>
2020-06-15 15:18:19 +0100

    Router and navigation bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, { Component } from 'react';
+import React, { Fragment, Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/NavBar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
+import About from './components/pages/About'
 import axios from 'axios';
 
 
@@ -37,19 +39,32 @@ class App extends Component {
   render() {
     const { users, loading } = this.state;
     return (
+      <Router>
       <div className='App'>
         <Navbar />  
         <div className='container'> 
         <Alert alert={this.state.alert} />  
-        <Search 
-          searchUsers={this.searchUsers} 
-          clearUsers={this.clearUsers} 
-          showClear={ users.length > 0 ? true : false }
-          setAlert={this.setAlert}
-          />
-        <Users loading={loading} users={users} />
+        <Switch>
+          <Route 
+            exact 
+            path='/' 
+            render={props => (
+            <Fragment>
+              <Search 
+                searchUsers={this.searchUsers} 
+                clearUsers={this.clearUsers} 
+                showClear={ users.length > 0 ? true : false }
+                setAlert={this.setAlert}
+              />
+              <Users loading={loading} users={users} />
+            </Fragment>
+          )} />
+          <Route exact path='/about' component={About} />
+        </Switch>
+       
       </div> 
       </div>   
+      </Router>
     );
   }
 }
---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react'
+
+export const About = () => {
+    return (
+        <Fragment>
+        <div>
+            <h1>About this App</h1>
+            <p>App to search Github users</p>
+        </div>
+        </Fragment>
+    )
+}
+
+export default About;
