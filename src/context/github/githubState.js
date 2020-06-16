02eca985d20935ef8b423518efd33d017286ad10
Francesca Chater <francescachater@gmail.com>
2020-06-16 16:10:33 +0100

    Move user repos to context state

--- src/context/github/githubState.js
+++ src/context/github/githubState.js
@@ -53,9 +53,23 @@ const GithubState = props => {
       } 
 
     // Get Repos
+    const getUserRepos = async username => {
+        setLoading();
+    
+        const response = await axios.get(
+          `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${
+          process.env.REACT_APP_GITHUB_CLIENT_ID
+        }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+        );
+    
+        dispatch({
+            type: GET_REPOS,
+            payload: response.data
+        })
+      }
 
     // Clear Users
-  const clearUsers = () => dispatch({ type: CLEAR_USERS })
+    const clearUsers = () => dispatch({ type: CLEAR_USERS })
 
     // Set Loading 
     const setLoading = () => dispatch({ type: SET_LOADING });
@@ -68,7 +82,8 @@ const GithubState = props => {
         loading: state.loading,
         searchUsers,
         clearUsers,
-        getUser
+        getUser,
+        getUserRepos
     }}
     >
     { props.children }
